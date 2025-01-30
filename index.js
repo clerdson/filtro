aa9dbc5af6b5b1f48f9c274bb31ff5ce4739f58f
clerdson <clerdson@clerdsons-MacBook-Air.local>
2025-01-29 23:51:08 -0300

    up

--- index.js
+++ index.js
@@ -129,6 +129,14 @@ const storage = multer.diskStorage({
     }
   
     try {
+      var sql = " CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL UNIQUE,password VARCHAR(255) NOT NULL,profile_image VARCHAR(255) NOT NULL);";
+      await db.execute(sql);
+
+
+
+      var sql = "CREATE TABLE measurements (id INT AUTO_INCREMENT PRIMARY KEY,user_id INT NOT NULL,local VARCHAR(255) NOT NULL, initial_mass FLOAT NOT NULL, final_mass FLOAT NOT NULL,image VARCHAR(255),created_at DATETIME NOT NULL,FOREIGN KEY (user_id) REFERENCES users(id));";
+      await db.execute(sql);
+      
       // Hash da senha
       const hashedPassword = await bcrypt.hash(password, 10);
   
